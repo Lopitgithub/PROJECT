9d5bb8c28b431b49fdb5be87d0fcd43d8c4d6fc5
Lopitgithub <140941178+Lopitgithub@users.noreply.github.com>
2024-07-09 21:53:02 +0545

    Add files via upload
    incomplete.

---
+++ OnlineVotingSystem/admin/inc/footer.php
@@ -0,0 +1,14 @@
+
+<div class="row bg-dark text-center text-white">
+            <div class="col-12 my-3">
+                 <p> &copy, copyright 2022 - All Right Reserved <br /> 
+                     Developed by ManishaGharti.com
+                </p>
+        </div>
+    </div>
+
+</div>
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ OnlineVotingSystem/admin/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once("inc/header.php");
+require_once("inc/navigation.php");
+?>
+
+<?php
+require_once("inc/footer.php");
+?>
